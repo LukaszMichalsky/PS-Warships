213854a44bb6e163bb39c03c4399de1d841e3ac3
Jakub Wyciślik <jakuwyc150@student.polsl.pl>
2020-11-09 18:00:01 +0100

    Added test code to main function

--- warships-new/Main.cpp
+++ warships-new/Main.cpp
@@ -2,10 +2,17 @@
 #include "xyio.h"
 
 int main() {
-	xyio::xyprintf(0, 0, "Hello World %d", 123);
-
 	Board testBoard(10);
-	testBoard.fillShips(); // Fills the board with empty ships (ShipState::EMPTY)
+	testBoard.fillShips(ShipState::STATE_EMPTY); // Fills the board with empty ships (ShipState::EMPTY)
+
+	testBoard.setShip(Ship(Point(2, 2), ShipState::STATE_NOT_HIT)); // New ship set at point (2, 2)
+	xyio::xyprintf(0, 0, "New ship set at point (2, 2)");
+
+	bool isValid = testBoard.isFieldValidForShip(Point(0, 0)); // Test whether new ship could be set at point (0, 0) --> should be true (1)
+	xyio::xyprintf(0, 1, "(0, 0) --> %d", isValid);
+
+	isValid = testBoard.isFieldValidForShip(Point(1, 1)); // Test whether new ship could be set at point (1, 1) --> should be false (0)
+	xyio::xyprintf(0, 2, "(1, 1) --> %d", isValid);
 
 	return 0;
 }
