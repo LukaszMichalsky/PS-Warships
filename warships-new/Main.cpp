573d951f722d791de8bbbd5b0b16276794153ada
Jakub Wyciślik <jakuwyc150@student.polsl.pl>
2020-11-13 13:06:57 +0100

    Removed test code from main() function, added application entry point

--- warships-new/Main.cpp
+++ warships-new/Main.cpp
@@ -1,18 +1,9 @@
 #include "board.h"
+#include "common.h"
 #include "xyio.h"
 
 int main() {
-	Board testBoard(10);
-	testBoard.fillShips(ShipState::STATE_EMPTY); // Fills the board with empty ships (ShipState::EMPTY)
-
-	testBoard.setShip(Ship(Point(2, 2), ShipState::STATE_NOT_HIT)); // New ship set at point (2, 2)
-	xyio::xyprintf(0, 0, "New ship set at point (2, 2)");
-
-	bool isValid = testBoard.isFieldValidForShip(Point(0, 0)); // Test whether new ship could be set at point (0, 0) --> should be true (1)
-	xyio::xyprintf(0, 1, "(0, 0) --> %d", isValid);
-
-	isValid = testBoard.isFieldValidForShip(Point(1, 1)); // Test whether new ship could be set at point (1, 1) --> should be false (0)
-	xyio::xyprintf(0, 2, "(1, 1) --> %d", isValid);
+	Common::drawMainMenu(); // Application entry point
 
 	return 0;
 }
