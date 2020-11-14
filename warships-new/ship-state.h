5e08c3d54243b30106753b806467ed244be45558
Jakub Wyciślik <jakuwyc150@student.polsl.pl>
2020-11-14 18:08:45 +0100

    Added new ship state

--- warships-new/ship-state.h
+++ warships-new/ship-state.h
@@ -3,6 +3,7 @@
 enum class ShipState {
 	STATE_EMPTY = 0x10, // Field is empty (no ship in it)
 	STATE_INVALID = 0x20, // An error occured, for example try to get ship from field with invalid coordinates
-	STATE_NOT_HIT = 0x30, // Field contains ship which is not hit yet
-	STATE_HIT = 0x40 // Field contains ship after being hit
+	STATE_MISSED_HIT = 0x30, // Field which was tried to hit, but there was no ship.
+	STATE_NOT_HIT = 0x40, // Field contains ship which is not hit yet
+	STATE_HIT = 0x50 // Field contains ship after being hit
 };
