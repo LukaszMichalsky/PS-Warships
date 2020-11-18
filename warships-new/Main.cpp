bf159394db0d351c4ee6f93c058357fc1abad366
Jakub Wyciślik <jakuwyc150@student.polsl.pl>
2020-11-18 18:53:03 +0100

    Added board creation example in main() function

--- warships-new/Main.cpp
+++ warships-new/Main.cpp
@@ -4,10 +4,11 @@
 
 int main() {
 	Common::drawMainMenu(); // Application entry point
-	//Board b;
-	
-	//b.drawBoard(1, 1);
-	
+
+	// Board b; // Creating default board (10x10)
+	// Board b(20); // Creating board with custom size (20x20)
+	// b.fillShips(ShipState::STATE_EMPTY); // Filling board with empty ships (fields)
+	// b.drawBoard(0, 0); // Draw board from point (0, 0) - top left pixel
 
 	return 0;
 }
